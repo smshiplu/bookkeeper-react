5877503673934686d1274ea5d5a9746a0d330732
SM-Nasir-Uddin_Shiplu <nasiruddinshiplu@gmail.com>
2024-02-22 20:16:45 +0600

    Chenges to Check API response from Rander backend

--- src/services/authService.js
+++ src/services/authService.js
@@ -5,6 +5,7 @@ export async function login(authDetail) {
     body: JSON.stringify(authDetail)
   }
   const response = await fetch(`${process.env.REACT_APP_HOST}/login`, requestOptions);
+  console.log(response);
   if(!response.ok) {
     throw {message: response.statusText, status: response.status} //eslint-disable-line
   }
