8a16493de32555faedfa865c39a0fae65cf8a210
SM-Nasir-Uddin_Shiplu <nasiruddinshiplu@gmail.com>
2024-02-22 19:47:17 +0600

    Changes for restart Vercel

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,7 @@
 export { Dashboard } from "./Dashboard/Dashboard";
 export { TransactionHistory } from "./TransactionHistory/TransactionHistory";
+export { Statement } from "./Statement/Statement";
+
 export { Login } from "./Login";
 export { Register } from "./Register";
-export { Statement } from "./Statement/Statement";
 export { PageNotFound } from "./PageNotFound";
